ab11255ca56a21d84b16daafdf53f555f1dc1113
ShuaipuZhao <82542747+ShuaipuZhao@users.noreply.github.com>
2021-09-03 19:00:46 +0930

    Recognize color
    Convert the image to binary image and detect the color.

--- RobotXColourVision/src/Color System.py
+++ RobotXColourVision/src/Color System.py
@@ -1,7 +1,6 @@
 import cv2
 import time
 import numpy as np
-import Image
 
 # define a dictionary containing the range of H(Hue), S(Saturation), V(Value) of red, green and blue
 color_dist = {"red": {"Lower": np.array([0, 60, 60]), "Upper": np.array([6, 255, 255])},
@@ -16,6 +15,7 @@ cap = cv2.VideoCapture(0)  # The parameter 0 in VideoCapture function is built-i
 # Named the window camera
 cv2.namedWindow("camera")
 
+# Set a parameter, defined as the number of frames
 timeF = 10
 c = 1
 
@@ -25,16 +25,38 @@ while cap.isOpened():
     # ret and frame are two return values. ret is a boolean, frame is every frame of image
     ret, frame = cap.read()
     if ret:
+        # ret and frame are two return values. ret is a boolean, frame is every frame of image
         ret, frame = cap.read()
+        # Make sure the frame is not none
         if frame is not None:
             if c % timeF == 0:
+
+                # Perform Gaussian Blur processing on the original image to facilitate color extraction.
+                # (5,5) is the width and length of Gaussian Matrix
+                # 0 is the standard of deviation
                 gs_frame = cv2.GaussianBlur(frame, (5, 5), 0)
-                hsv = cv2cvtColor(gas_frame, cv2.COLOR_BGR2HSV)
+
+                # Convert Gaussian Blur image from BRG to HSV is more suitable for single color extraction.
+                # OpenCV used rather BRG than RGB
+                hsv = cv2.cvtColor(gs_frame, cv2.COLOR_BGR2HSV)
+
+                # Make the image thinner and remover the noise in the image
+                # Iteration represent the erode width
                 erode_hsv = cv2.erode(hsv, None, iterations=2)
-                cv2.imwrite("./Camera_image" + str(c/10) + '.jpg', frame)
+
+                # Remove other colors in the recognized colors convert the image as a binary image
+                inRange_hsv = cv2.inRange(erode_hsv, color_dist['green']['Lower'], color_dist['green']['Upper'])
+
+                # Save the binary image as a record to test
+                cv2.imwrite("./Camera_image" + str(c/10) + '.jpg', inRange_hsv)
+
             c += 1
+
+            # Show the frame
             cv2.imshow("camera", frame)
             k = cv2.waitKey(1)
+
+            # If the 'q' key was pressed, break the loop
             if k == ord('q'):
                 cap.release()
                 break
