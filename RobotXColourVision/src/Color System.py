ce8eb57b39219402c11be818ba4150696a9de917
ShuaipuZhao <82542747+ShuaipuZhao@users.noreply.github.com>
2021-09-04 16:45:02 +0930

    judge the image
    judge the image is red, blue or green

--- RobotXColourVision/src/Color System.py
+++ RobotXColourVision/src/Color System.py
@@ -15,9 +15,11 @@ cap = cv2.VideoCapture(0)  # The parameter 0 in VideoCapture function is built-i
 # Named the window camera
 cv2.namedWindow("camera")
 
+font = cv2.FONT_HERSHEY_SIMPLEX
 # Set a parameter, defined as the number of frames
 timeF = 10
 c = 1
+# num = 0
 
 # Determine the camera status. If return true means camera turn on success, false means fail.
 while cap.isOpened():
@@ -45,16 +47,28 @@ while cap.isOpened():
                 erode_hsv = cv2.erode(hsv, None, iterations=2)
 
                 # Remove other colors in the recognized colors convert the image as a binary image
-                inRange_hsv = cv2.inRange(erode_hsv, color_dist['green']['Lower'], color_dist['green']['Upper'])
+                inRange_hsv_green = cv2.inRange(erode_hsv, color_dist['green']['Lower'], color_dist['green']['Upper'])
+                inRange_hsv_red = cv2.inRange(erode_hsv, color_dist['red']['Lower'], color_dist['red']['Upper'])
+                inRange_hsv_blue = cv2.inRange(erode_hsv, color_dist['blue']['Lower'], color_dist['blue']['Upper'])
 
+                result1 = not np.any(cv2.subtract(inRange_hsv_green, inRange_hsv_red))
+                result2 = not np.any(cv2.subtract(inRange_hsv_green, inRange_hsv_blue))
+                result3 = not np.any(cv2.subtract(inRange_hsv_blue, inRange_hsv_red))
 
-               
-                cnt = cv2.findContours(inRange_hsv, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-
-                # Save the binary image as a record to test
-                cv2.imwrite("./Camera_image" + str(c / 10) + '.jpg', inRange_hsv)
-
+                if result1 == result2 == result3 == True:
+                    print("None")
+                if result1 == result2 == False & result3 == True:
+                    print("green")
+                    # Save the binary image as a record to test
+                    cv2.imwrite("./Camera_image" + str(c / 10) + '.jpg', inRange_hsv_green)
+                if result1 == result3 == False & result2 == True:
+                    print("red")
+                    # Save the binary image as a record to test
+                    cv2.imwrite("./Camera_image" + str(c / 10) + '.jpg', inRange_hsv_red)
+                if result2 == result3 == False & result1 == True:
+                    print("blue")
+                    # Save the binary image as a record to test
+                    cv2.imwrite("./Camera_image" + str(c / 10) + '.jpg', inRange_hsv_blue)
             c += 1
 
             # Show the frame
