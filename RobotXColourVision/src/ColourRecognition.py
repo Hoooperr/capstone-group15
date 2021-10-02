5d617aacdf2b2c645124101b18654b6fb9dc7fef
Tom Hooper <hooth002@mymail.unisa.edu.au>
2021-10-02 11:46:43 +0930

    Moved colour recognition code into its own class
    This will enable code reuse for both the colour sequence and rectangle recognition aspects of the system.

---
+++ RobotXColourVision/src/ColourRecognition.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+def findRGBContours(frame):
+    # define a dictionary containing the range of H(Hue), S(Saturation), V(Value) of red, green and blue
+    color_dist = {"red": {"Lower": np.array([0, 150, 60]), "Upper": np.array([10, 255, 255])},
+                  "blue": {"Lower": np.array([95, 100, 60]), "Upper": np.array([120, 255, 255])},
+                  "green": {"Lower": np.array([30, 60, 60]), "Upper": np.array([75, 255, 255])}
+                  }
+    # masking
+    blurred = cv2.GaussianBlur(frame, (3, 3), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    kernel = np.ones((5, 5), np.uint8)
+    erode = cv2.erode(hsv, kernel)
+
+    # allows only objects of red, green or blue to appear in their respective frame
+    hsv_red = cv2.inRange(erode, color_dist['red']['Lower'], color_dist['red']['Upper'])
+    hsv_blue = cv2.inRange(erode, color_dist['blue']['Lower'], color_dist['blue']['Upper'])
+    hsv_green = cv2.inRange(erode, color_dist['green']['Lower'], color_dist['green']['Upper'])
+
+    # get all contours for each colour
+    contours_red, heir_red = cv2.findContours(hsv_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_blue, heir_blue = cv2.findContours(hsv_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_green, heir_green = cv2.findContours(hsv_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    return contours_red, contours_blue, contours_green
--- RobotXColourVision/src/RectangleVision.py
+++ RobotXColourVision/src/RectangleVision.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from ColourRecognition import findRGBContours
 
 def distanceToObject(known_width, focal_length, pixel_width):
     return (known_width * focal_length) / pixel_width
@@ -8,46 +9,28 @@ def isVerticalLine(pos1, pos2):
     return True if np.absolute(pos1[0] - pos2[0]) < np.absolute(pos1[1] - pos2[1]) else False
 
 
-# define a dictionary containing the range of H(Hue), S(Saturation), V(Value) of red, green and blue
-color_dist = {"red": {"Lower": np.array([0, 150, 60]), "Upper": np.array([10, 255, 255])},
-              "blue": {"Lower": np.array([95, 100, 60]), "Upper": np.array([120, 255, 255])},
-              "green": {"Lower": np.array([30, 60, 60]), "Upper": np.array([75, 255, 255])}
-              }
-
 cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
     ret, frame = cap.read()
 
     if ret:
-        # masking
-        blurred = cv2.GaussianBlur(frame, (3, 3), 0)
-        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-        kernel = np.ones((5, 5), np.uint8)
-        erode = cv2.erode(hsv, kernel)
-
-        # allows only objects of red, green or blue to appear in their respective frame
-        hsv_red = cv2.inRange(erode, color_dist['red']['Lower'], color_dist['red']['Upper'])
-        hsv_blue = cv2.inRange(erode, color_dist['blue']['Lower'], color_dist['blue']['Upper'])
-        hsv_green = cv2.inRange(erode, color_dist['green']['Lower'], color_dist['green']['Upper'])
 
         # get all contours for each colour
-        cnts_red, heir_red = cv2.findContours(hsv_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cnts_blue, heir_blue = cv2.findContours(hsv_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cnts_green, heir_green = cv2.findContours(hsv_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours_red, contours_blue, contours_green = findRGBContours(frame)
 
         largest_contour_area = None
 
         # find contour with largest area
-        for contour in cnts_red:
+        for contour in contours_red:
             if largest_contour_area is None or cv2.contourArea(contour) > cv2.contourArea(largest_contour_area[0]):
                 largest_contour_area = (contour, "red")
 
-        for contour in cnts_blue:
+        for contour in contours_blue:
             if largest_contour_area is None or cv2.contourArea(contour) > cv2.contourArea(largest_contour_area[0]):
                 largest_contour_area = (contour, "blue")
 
-        for contour in cnts_green:
+        for contour in contours_green:
             if largest_contour_area is None or cv2.contourArea(contour) > cv2.contourArea(largest_contour_area[0]):
                 largest_contour_area = (contour, "green")
 
@@ -81,7 +64,7 @@ while cap.isOpened():
                 distance = distanceToObject(24, 520, width)
 
                 # draw rectangle contour to frame
-                cv2.drawContours(frame, np.array(poly_approx), 0, (0, 255, 0), 2)
+                cv2.drawContours(frame, [poly_approx], 0, (0, 255, 0), 2)
                 cv2.putText(frame, "rectangle", (x + w + 10, y + h), 0, 0.5, (0, 255, 0))
                 cv2.putText(frame, "%.2fcm" % distance, [int(x+(w/2)), int(y+(h/2))], 0, 0.5, (0, 255, 0))
 
