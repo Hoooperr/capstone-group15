62a1b469711b859a43c0d9decc984f2192584cce
ShuaipuZhao <82542747+ShuaipuZhao@users.noreply.github.com>
2021-10-07 16:34:55 +1030

    color sequence
    add the color black and detect the color according black. the color sequence is between the two black.

--- RobotXColourVision/src/Color System.py
+++ RobotXColourVision/src/Color System.py
@@ -1,10 +1,12 @@
 import cv2
+import time
 import numpy as np
 
 # define a dictionary containing the range of H(Hue), S(Saturation), V(Value) of red, green and blue
 color_dist = {"red": {"Lower": np.array([0, 200, 60]), "Upper": np.array([10, 255, 255])},
               "blue": {"Lower": np.array([90, 60, 60]), "Upper": np.array([120, 255, 255])},
-              "green": {"Lower": np.array([40, 40, 40]), "Upper": np.array([75, 255, 255])}
+              "green": {"Lower": np.array([40, 40, 40]), "Upper": np.array([75, 255, 255])},
+              "black": {"Lower": np.array([0, 0, 0]), "Upper": np.array([180, 255, 46])}
               }
 
 # Indicates the camera is turned on.
@@ -18,12 +20,16 @@ result = 0
 area_blue = 0
 area_red = 0
 area_green = 0
+area_black = 0
 max_area = 0
-array = ["black"]
 count = 0
+list = []
+
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 # Set a parameter, defined as the number of frames
+timeF = 10
+c = 1
 
 # Determine the camera status. If return true means camera turn on success, false means fail.
 while cap.isOpened():
@@ -35,130 +41,121 @@ while cap.isOpened():
         ret, frame = cap.read()
         # Make sure the frame is not none
         if frame is not None:
+            if c % timeF == 0:
 
-            # Perform Gaussian Blur processing on the original image to facilitate color extraction.
-            # (5,5) is the width and length of Gaussian Matrix
-            # 0 is the standard of deviation
-            gs_frame = cv2.GaussianBlur(frame, (5, 5), 0)
+                # Perform Gaussian Blur processing on the original image to facilitate color extraction.
+                # (5,5) is the width and length of Gaussian Matrix
+                # 0 is the standard of deviation
+                gs_frame = cv2.GaussianBlur(frame, (5, 5), 0)
 
-            # Convert Gaussian Blur image from BRG to HSV is more suitable for single color extraction.
-            # OpenCV used rather BRG than RGB
-            hsv = cv2.cvtColor(gs_frame, cv2.COLOR_BGR2HSV)
+                # Convert Gaussian Blur image from BRG to HSV is more suitable for single color extraction.
+                # OpenCV used rather BRG than RGB
+                hsv = cv2.cvtColor(gs_frame, cv2.COLOR_BGR2HSV)
 
-            # Make the image thinner and remover the noise in the image
-            # Iteration represent the erode width
-            erode_hsv = cv2.erode(hsv, None, iterations=2)
+                # Make the image thinner and remover the noise in the image
+                # Iteration represent the erode width
+                erode_hsv = cv2.erode(hsv, None, iterations=2)
 
-            # Remove other colors in the recognized colors convert the image as a binary image
-            inRange_hsv_green = cv2.inRange(erode_hsv, color_dist['green']['Lower'], color_dist['green']['Upper'])
-            inRange_hsv_red = cv2.inRange(erode_hsv, color_dist['red']['Lower'], color_dist['red']['Upper'])
-            inRange_hsv_blue = cv2.inRange(erode_hsv, color_dist['blue']['Lower'], color_dist['blue']['Upper'])
+                # Remove other colors in the recognized colors convert the image as a binary image
+                inRange_hsv_green = cv2.inRange(erode_hsv, color_dist['green']['Lower'], color_dist['green']['Upper'])
+                inRange_hsv_red = cv2.inRange(erode_hsv, color_dist['red']['Lower'], color_dist['red']['Upper'])
+                inRange_hsv_blue = cv2.inRange(erode_hsv, color_dist['blue']['Lower'], color_dist['blue']['Upper'])
+                inRange_hsv_black = cv2.inRange(erode_hsv, color_dist['black']['Lower'], color_dist['black']['Upper'])
 
-            # Use findContours function to detect the binary image to reconigize the area
-            contours_green, hierarchy_green = cv2.findContours(inRange_hsv_green, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-            contours_red, hierarchy_red = cv2.findContours(inRange_hsv_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-            contours_blue, hierarchy_blue = cv2.findContours(inRange_hsv_blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                # Use findContours function to detect the binary image to recognize the area
+                contours_green, hierarchy_green = cv2.findContours(inRange_hsv_green, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                contours_red, hierarchy_red = cv2.findContours(inRange_hsv_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                contours_blue, hierarchy_blue = cv2.findContours(inRange_hsv_blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                contours_black, hierarchy_black = cv2.findContours(inRange_hsv_black, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-            # Define the initial area
-            area_blue = 0
-            area_red = 0
-            area_green = 0
+                # Define the initial area
+                area_blue = 0
+                area_red = 0
+                area_green = 0
+                area_black = 0
 
-            # Calculate the area of different color in binary inage
-            for i in contours_blue:
-                area_blue += cv2.contourArea(i)
+                # Calculate the area of different color in binary inage
+                for i in contours_blue:
+                    area_blue += cv2.contourArea(i)
 
-            for i in contours_green:
-                area_green += cv2.contourArea(i)
+                for i in contours_black:
+                    area_black += cv2.contourArea(i)
 
-            for i in contours_red:
-                area_red += cv2.contourArea(i)
+                for i in contours_green:
+                    area_green += cv2.contourArea(i)
 
-            # print the color of max area in image
-            result = max(area_blue, area_green, area_red)
+                for i in contours_red:
+                    area_red += cv2.contourArea(i)
 
-            #for j in range(3):
+                # print the color of max area in image
+                result = max(area_blue, area_green, area_red, area_black)
 
-            if result == area_blue:
-                if array[count] != "blue":
-                    if count >= 3:
-                            newAr = []
-                            newAr.append("blue")
-                            newAr.append(array[0])
-                            newAr.append(array[1])
-                            array[0] = newAr[0]
-                            array[1] = newAr[1]
-                            array[2] = newAr[2]
-                            count = 3
-                            #array.append("blue")
-                        else:
-                            array.append("blue")
-                            count=count + 1
-                    print(array)
+                if result == area_blue:
+                    if len(list) == 0:
+                        list.append("blue")
+                    else:
+                        if list[-1] != "blue":
+                            list.append("blue")
 
                 if result == area_red:
-                    if array[count] != "red":
-                        if count >= 3:
-                            newAr = []
-                            newAr.append("red")
-                            newAr.append(array[0])
-                            newAr.append(array[1])
-                            array[0] = newAr[0]
-                            array[1] = newAr[1]
-                            array[2] = newAr[2]
-                            count = 3
-                            #array.append("blue")
-                        else:
-                            array.append("red")
-                            count=count + 1
-                    print(array)
+                    if len(list) == 0:
+                        list.append("red")
+                    else:
+                        if list[-1] != "red":
+                            list.append("red")
 
                 if result == area_green:
-                    if array[count] != "green":
-                        if count >= 3:
-                            newAr = []
-                            newAr.append("green")
-                            newAr.append(array[0])
-                            newAr.append(array[1])
-                            array[0] = newAr[0]
-                            array[1] = newAr[1]
-                            array[2] = newAr[2]
-                            count = 3
-                            #array.append("blue")
-                        else:
-                            array.append("green")
-                            count=count + 1
-                    print(array)
+                    if len(list) == 0:
+                        list.append("green")
+                    else:
+                        if list[-1] != "green":
+                            list.append("green")
+
+                if result == area_black:
+                    if len(list) == 0:
+                        list.append("black")
+                    else:
+                        if list[-1] != "black":
+                            list.append("black")
+
+                index1 = list.index("black")
+                index2 = list.index("black", index1+1)
+                sequence = list[index1:index2]
+                print(sequence)
+
 
                 # Identify the color border
                 for cnt in contours_green:
                     (x, y, w, h) = cv2.boundingRect(cnt)
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
-                    #print("green")
                     # Save the binary image as a record to test
                     cv2.imwrite("./Camera_image" + str(c / 10) + '.jpg', inRange_hsv_green)
+
+                for cnt in contours_black:
+                    (x, y, w, h) = cv2.boundingRect(cnt)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+                    # Save the binary image as a record to test
+                    cv2.imwrite("./Camera_image" + str(c / 10) + '.jpg', inRange_hsv_black)
 
                 for cnt1 in contours_blue:
                     (x1, y1, w1, h1) = cv2.boundingRect(cnt1)
                     cv2.rectangle(frame, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 255), 2)
-                    #print("blue")
                     # Save the binary image as a record to test
                     cv2.imwrite("./Camera_image" + str(c / 10) + '.jpg', inRange_hsv_blue)
 
                 for cnt2 in contours_red:
                     (x2, y2, w2, h2) = cv2.boundingRect(cnt2)
                     cv2.rectangle(frame, (x2, y2), (x2 + w2, y2 + h2), (0, 255, 255), 2)
-                    #print("red")
                     # Save the binary image as a record to test
                     cv2.imwrite("./Camera_image" + str(c / 10) + '.jpg', inRange_hsv_red)
 
                 #cv2.imshow("green", inRange_hsv_green)
                 #cv2.imshow("blue", inRange_hsv_blue)
                 #cv2.imshow("red", inRange_hsv_red)
+                #cv2.imshow("black", inRange_hsv_black)
             c += 1
 
             # Show the frame
-
             cv2.imshow("camera", frame)
             k = cv2.waitKey(1)
 
