e9aa7378839c30aa2de7b7d990b27f90595d7e09
ShuaipuZhao <82542747+ShuaipuZhao@users.noreply.github.com>
2021-10-07 13:50:10 +1030

    try to find the sequence
    add array to save the sequence of color

--- RobotXColourVision/src/Color System.py
+++ RobotXColourVision/src/Color System.py
@@ -1,5 +1,4 @@
 import cv2
-import time
 import numpy as np
 
 # define a dictionary containing the range of H(Hue), S(Saturation), V(Value) of red, green and blue
@@ -20,11 +19,11 @@ area_blue = 0
 area_red = 0
 area_green = 0
 max_area = 0
+array = ["black"]
+count = 0
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 # Set a parameter, defined as the number of frames
-timeF = 10
-c = 1
 
 # Determine the camera status. If return true means camera turn on success, false means fail.
 while cap.isOpened():
@@ -36,56 +35,100 @@ while cap.isOpened():
         ret, frame = cap.read()
         # Make sure the frame is not none
         if frame is not None:
-            if c % timeF == 0:
 
-                # Perform Gaussian Blur processing on the original image to facilitate color extraction.
-                # (5,5) is the width and length of Gaussian Matrix
-                # 0 is the standard of deviation
-                gs_frame = cv2.GaussianBlur(frame, (5, 5), 0)
+            # Perform Gaussian Blur processing on the original image to facilitate color extraction.
+            # (5,5) is the width and length of Gaussian Matrix
+            # 0 is the standard of deviation
+            gs_frame = cv2.GaussianBlur(frame, (5, 5), 0)
 
-                # Convert Gaussian Blur image from BRG to HSV is more suitable for single color extraction.
-                # OpenCV used rather BRG than RGB
-                hsv = cv2.cvtColor(gs_frame, cv2.COLOR_BGR2HSV)
+            # Convert Gaussian Blur image from BRG to HSV is more suitable for single color extraction.
+            # OpenCV used rather BRG than RGB
+            hsv = cv2.cvtColor(gs_frame, cv2.COLOR_BGR2HSV)
 
-                # Make the image thinner and remover the noise in the image
-                # Iteration represent the erode width
-                erode_hsv = cv2.erode(hsv, None, iterations=2)
+            # Make the image thinner and remover the noise in the image
+            # Iteration represent the erode width
+            erode_hsv = cv2.erode(hsv, None, iterations=2)
 
-                # Remove other colors in the recognized colors convert the image as a binary image
-                inRange_hsv_green = cv2.inRange(erode_hsv, color_dist['green']['Lower'], color_dist['green']['Upper'])
-                inRange_hsv_red = cv2.inRange(erode_hsv, color_dist['red']['Lower'], color_dist['red']['Upper'])
-                inRange_hsv_blue = cv2.inRange(erode_hsv, color_dist['blue']['Lower'], color_dist['blue']['Upper'])
+            # Remove other colors in the recognized colors convert the image as a binary image
+            inRange_hsv_green = cv2.inRange(erode_hsv, color_dist['green']['Lower'], color_dist['green']['Upper'])
+            inRange_hsv_red = cv2.inRange(erode_hsv, color_dist['red']['Lower'], color_dist['red']['Upper'])
+            inRange_hsv_blue = cv2.inRange(erode_hsv, color_dist['blue']['Lower'], color_dist['blue']['Upper'])
 
-                # Use findContours function to detect the binary image to reconigize the area
-                contours_green, hierarchy_green = cv2.findContours(inRange_hsv_green, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-                contours_red, hierarchy_red = cv2.findContours(inRange_hsv_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-                contours_blue, hierarchy_blue = cv2.findContours(inRange_hsv_blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            # Use findContours function to detect the binary image to reconigize the area
+            contours_green, hierarchy_green = cv2.findContours(inRange_hsv_green, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            contours_red, hierarchy_red = cv2.findContours(inRange_hsv_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            contours_blue, hierarchy_blue = cv2.findContours(inRange_hsv_blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-                # Define the initial area
-                area_blue = 0
-                area_red = 0
-                area_green = 0
+            # Define the initial area
+            area_blue = 0
+            area_red = 0
+            area_green = 0
 
-                # Calculate the area of different color in binary inage
-                for i in contours_blue:
-                    area_blue += cv2.contourArea(i)
+            # Calculate the area of different color in binary inage
+            for i in contours_blue:
+                area_blue += cv2.contourArea(i)
 
-                for i in contours_green:
-                    area_green += cv2.contourArea(i)
+            for i in contours_green:
+                area_green += cv2.contourArea(i)
 
-                for i in contours_red:
-                    area_red += cv2.contourArea(i)
+            for i in contours_red:
+                area_red += cv2.contourArea(i)
 
-                # print the color of max area in image
-                result = max(area_blue, area_green, area_red)
-                if result == area_blue:
-                    print("blue")
+            # print the color of max area in image
+            result = max(area_blue, area_green, area_red)
+
+            #for j in range(3):
+
+            if result == area_blue:
+                if array[count] != "blue":
+                    if count >= 3:
+                            newAr = []
+                            newAr.append("blue")
+                            newAr.append(array[0])
+                            newAr.append(array[1])
+                            array[0] = newAr[0]
+                            array[1] = newAr[1]
+                            array[2] = newAr[2]
+                            count = 3
+                            #array.append("blue")
+                        else:
+                            array.append("blue")
+                            count=count + 1
+                    print(array)
 
                 if result == area_red:
-                    print("red")
+                    if array[count] != "red":
+                        if count >= 3:
+                            newAr = []
+                            newAr.append("red")
+                            newAr.append(array[0])
+                            newAr.append(array[1])
+                            array[0] = newAr[0]
+                            array[1] = newAr[1]
+                            array[2] = newAr[2]
+                            count = 3
+                            #array.append("blue")
+                        else:
+                            array.append("red")
+                            count=count + 1
+                    print(array)
 
                 if result == area_green:
-                    print("green")
+                    if array[count] != "green":
+                        if count >= 3:
+                            newAr = []
+                            newAr.append("green")
+                            newAr.append(array[0])
+                            newAr.append(array[1])
+                            array[0] = newAr[0]
+                            array[1] = newAr[1]
+                            array[2] = newAr[2]
+                            count = 3
+                            #array.append("blue")
+                        else:
+                            array.append("green")
+                            count=count + 1
+                    print(array)
 
                 # Identify the color border
                 for cnt in contours_green:
